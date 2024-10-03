9a5393fc93abcc712c1ae33e76a0423b3ce1bfc8
Atakan Yel <atakan.yel@hotmail.com>
2024-10-03 14:05:01 +0300

    a

---
+++ src/main/java/tr/edu/ogu/ceng/Order/OrderApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.ogu.ceng.Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
